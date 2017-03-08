6ec55b168108bdb04129986c8c6eb9ac1b95dd39
Divam <dfordivam@gmail.com>
2017-03-08 10:15:44 +0900

    compute radius of circle

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,15 +15,18 @@ main = do
   putStrLn "Hello"
   let sizeSpec = mkSizeSpec2D (Just 600) (Nothing)
 
-  (Right img') <- loadImageEmb "00001.png"
+  (Right img') <- loadImageEmb "0003.png"
   let 
       origImg :: Diagram B
       origImg = image img'
 
       scaledImg = scale 0.5 origImg
 
+      num = 32
+      radius = getDefaults (width scaledImg, height scaledImg) num
   
-      finalImg = getImageAndPosition scaledImg 4 200 (-45)
+      rotOffset = 0
+      finalImg = getImageAndPosition scaledImg num radius rotOffset
 
 
 
@@ -48,3 +51,16 @@ getImageAndPosition img num radius rotOffset = mconcat $ map snd transList
           x n = g sin n
           y n = g cos n
           g f n = radius*f ((((-2)*(fromIntegral n))/(fromIntegral num))*pi)
+
+
+-- Default parameters determination
+--
+-- alpha : angle between two patterns = (360/num)
+--
+-- Radius = (height/2) + (width/2)*(cot (alpha/2))
+
+getDefaults :: (Double, Double) -> Int -> Double
+getDefaults (w,h) num' = (h/2) + (w/2)/(tan (alpha/2))
+  where
+    num = fromIntegral num'
+    alpha = (2*pi)/num
